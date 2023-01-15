03bc29e06426ceead64e286003036d01a75c838a
gustonecrush <codeboxezz@gmail.com>
2023-01-15 16:38:24 +0700

    setting up tailwindcss, google font, and created header framework section

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="w-full fixed">
+      <div className="py-4 font-poppins max-w-7xl bg-blue-100 mx-auto flex flex-row justify-between">
+        {/* Header Menu Section */}
+        <div>kiri</div>
+        {/* Social Media and CV Section */}
+        <div>kanan</div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
